fcf1dbad47648e6314e0d09d6504a38886078dca
Crazy-Owl <stalker401@gmail.com>
2017-02-02 22:12:05 +0300

    rust fmt this, rust fmt that

--- src/lib.rs
+++ src/lib.rs
@@ -2,26 +2,29 @@ extern crate sdl2;
 
 use std::collections::linked_list::LinkedList;
 
-
 // Game Engine
 
 pub struct Engine {
     pub model: Model,
-    pub messages: LinkedList<Msg>
+    pub messages: LinkedList<Msg>,
 }
 
+/// Basic trait for all game engines.
 pub trait TEngine {
     type Message;
     type Model;
 
-    fn update(&mut self, msg : Self::Message) -> Option<Self::Message>;
+    fn update(&mut self, msg: Self::Message) -> Option<Self::Message>;
     fn render(&self);
     fn process(&mut self) -> bool;
 }
 
 impl Engine {
     pub fn new() -> Engine {
-        Engine { model: Model::new(), messages: LinkedList::new()}
+        Engine {
+            model: Model::new(),
+            messages: LinkedList::new(),
+        }
     }
 }
 
@@ -29,13 +32,13 @@ impl TEngine for Engine {
     type Message = Msg;
     type Model = Model;
 
-    fn update(&mut self, msg : Msg) -> Option<Msg> {
+    fn update(&mut self, msg: Msg) -> Option<Msg> {
         match msg {
             Msg::NoOp => None,
             Msg::Exit => {
                 self.model.state = false;
                 None
-            },
+            }
             Msg::Change(x) => {
                 self.model.message = x;
                 Some(Msg::Exit)
@@ -61,16 +64,23 @@ impl TEngine for Engine {
 #[derive(Debug)]
 pub struct Model {
     pub state: bool,
-    pub message: String
+    pub message: String,
 }
 
 impl Model {
     pub fn new() -> Model {
-        Model {state: true, message: "Hello world".to_string()}
+        Model {
+            state: true,
+            message: "Hello world".to_string(),
+        }
     }
 }
 
 // Msg
 
 #[derive(Debug)]
-pub enum Msg { NoOp, Exit, Change(String) }
+pub enum Msg {
+    NoOp,
+    Exit,
+    Change(String),
+}
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate rogue;
 use rogue::{Engine, Msg, TEngine};
 
 fn main() {
-    let mut engine : Engine = Engine::new();
+    let mut engine: Engine = Engine::new();
     engine.messages.push_back(Msg::Change("Kek".to_string()));
     engine.messages.push_back(Msg::NoOp);
     'running: loop {
