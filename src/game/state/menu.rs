d070cba4475f117655ccec8bf3e80e5a1d76523c
Crazy-Owl <stalker401@gmail.com>
2017-02-24 21:02:36 +0300

    add text rendering to texture

--- src/game/state/menu.rs
+++ src/game/state/menu.rs
@@ -1,17 +1,38 @@
 use sdl2::render::{Texture, Renderer};
+use sdl2::ttf::Font;
+use sdl2::pixels::Color::*;
 use msg::Msg;
 use model::Model;
 use engine::state::StateT;
 
+struct MenuItem {
+    text: String,
+    texture: Texture,
+    msg: Msg,
+}
+
 struct MenuState {
-    choices: Vec<(String, Texture, Msg)>,
+    menu_items: Vec<MenuItem>,
+    currently_selected: usize,
     top_left: (i32, i32),
 }
 
-impl MenuState {
-    fn new() -> MenuState {
+impl<'m> MenuState {
+    fn new(f: &Font<'m, 'static>, r: &mut Renderer, choices: Vec<(String, Msg)>) -> MenuState {
+        let mut menu_items = Vec::new();
+        for choice in choices {
+            let surface =
+                f.render(&choice.0).solid(RGB(255, 255, 255)).expect("Could not render text!");
+            let texture = r.create_texture_from_surface(&surface).expect("Could not render text!");
+            menu_items.push(MenuItem {
+                text: choice.0,
+                texture: texture,
+                msg: choice.1,
+            });
+        }
         MenuState {
-            choices: Vec::new(),
+            menu_items: menu_items,
+            currently_selected: 0,
             top_left: (0, 0),
         }
     }
@@ -25,7 +46,5 @@ impl StateT for MenuState {
         None
     }
 
-    fn render(&mut self, r: &mut Renderer) {
-
-    }
+    fn render(&mut self, r: &mut Renderer) {}
 }
