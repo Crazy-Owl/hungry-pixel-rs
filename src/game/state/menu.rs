1a58f2ad54a908f1ff45791b73936eea1e64fb1b
Crazy-Owl <stalker401@gmail.com>
2017-02-24 21:20:49 +0300

    added menu rendering

--- src/game/state/menu.rs
+++ src/game/state/menu.rs
@@ -1,4 +1,5 @@
 use sdl2::render::{Texture, Renderer};
+use sdl2::rect::Rect;
 use sdl2::ttf::Font;
 use sdl2::pixels::Color::*;
 use msg::Msg;
@@ -8,6 +9,7 @@ use engine::state::StateT;
 struct MenuItem {
     text: String,
     texture: Texture,
+    dimensions: (u32, u32),
     msg: Msg,
 }
 
@@ -24,16 +26,18 @@ impl<'m> MenuState {
             let surface =
                 f.render(&choice.0).solid(RGB(255, 255, 255)).expect("Could not render text!");
             let texture = r.create_texture_from_surface(&surface).expect("Could not render text!");
+            let query = texture.query();
             menu_items.push(MenuItem {
                 text: choice.0,
                 texture: texture,
+                dimensions: (query.width, query.height),
                 msg: choice.1,
             });
         }
         MenuState {
             menu_items: menu_items,
             currently_selected: 0,
-            top_left: (0, 0),
+            top_left: (60, 0),
         }
     }
 }
@@ -46,5 +50,17 @@ impl StateT for MenuState {
         None
     }
 
-    fn render(&mut self, r: &mut Renderer) {}
+    fn render(&mut self, r: &mut Renderer) {
+        let mut current_y: u32 = self.top_left.1 as u32;
+        let mut running_counter: usize = 0;
+        for item in &self.menu_items {
+            r.copy(&item.texture, None, Some(Rect::new(current_y as i32, self.top_left.0 as i32, item.dimensions.0, item.dimensions.1))).unwrap();
+            current_y += item.dimensions.1 + 2;
+            if running_counter == self.currently_selected {
+                r.set_draw_color(RGB(255, 255, 255));
+                r.draw_rect(Rect::new(self.top_left.0 - 20, current_y as i32, 20, item.dimensions.1)).unwrap();
+            }
+            running_counter += 1;
+        }
+    }
 }
