a6c292a9c9c1f1b12d9db2dcd6e56b1e47b2a178
Crazy-Owl <stalker401@gmail.com>
2017-03-13 22:46:58 +0300

    fix typo in build script

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,6 +7,6 @@ cp /usr/lib/x86_64-linux-gnu/libSDL2_ttf-2.0.so.0 ./release
 cp /usr/lib/x86_64-linux-gnu/libSDL2_image-2.0.so.0 ./release
 cp /usr/local/lib/libSDL2-2.0.so.0 ./release
 cp -R resources ./release
-VER_NUM = `git describe --abbrev=0 --tags`
+VER_NUM=`git describe --abbrev=0 --tags`
 tar -zcvf hungry-pixel-rs-$VER_NUM-linux-64bit.tar.gz ./release
 rm -rf ./release
